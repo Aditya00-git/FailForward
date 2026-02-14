d261283336d9066019f464149c2a800b77e8b299
Aditya <seswaniaditya@gmail.com>
2026-02-14 18:20:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -108,7 +108,7 @@ app.get("/auth/google/callback",
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback"
+  callbackURL: process.env.BASE_URL + "/auth/google/callback"
 },
 async (accessToken, refreshToken, profile, done) => {
   let user = await User.findOne({ email: profile.emails[0].value });
